9f7281cb85db6f86865b802128ed66f8b4056140
Daniel Morfa Vega <dmorfav@gmail.com>
2023-07-10 00:08:29 +0200

    feature(optimization): add service-worker for cache optimization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: true, //!isDevMode(),
+      enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:10000'
