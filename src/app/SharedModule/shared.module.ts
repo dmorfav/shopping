462377f4f83dd00e37312bbbed2ebd4680622291
Daniel Morfa Vega <dmorfav@gmail.com>
2023-07-12 00:24:04 +0200

    refactor(optimization): move service declaration
    add service declaration to shared module

--- src/app/SharedModule/shared.module.ts
+++ src/app/SharedModule/shared.module.ts
@@ -1,13 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ProductService} from "../Services/product.service";
+import {ResProductService} from "../IO/res-product.service";
+import {CategoryService} from "../Services/category.service";
+import {ResCategoryService} from "../IO/res-category.service";
+import {WorkerCommunicationService} from "../Services/worker-communication.service";
 
 
 @NgModule({
   declarations: [],
+  providers: [
+    ProductService,
+    ResProductService,
+    CategoryService,
+    ResCategoryService,
+    WorkerCommunicationService
+  ],
   exports: [],
   imports: [
     CommonModule
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
