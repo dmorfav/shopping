d727e4b92259e5c9b712ccde7c4fe5e3a48eaa17
Daniel Morfa Vega <dmorfav@gmail.com>
2023-07-12 16:07:31 +0200

    fix(component): added load products every time component is accessed

--- src/app/HomeModule/home/home.component.ts
+++ src/app/HomeModule/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {ProductService} from "../../Services/product.service";
 
 @Component({
@@ -6,7 +6,7 @@ import {ProductService} from "../../Services/product.service";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   readonly carouselsImg: string[] = [
     'https://images.unsplash.com/photo-1422190441165-ec2956dc9ecc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80',
     'https://images.unsplash.com/photo-1533090161767-e6ffed986c88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjM0MTM2fQ&auto=format&fit=crop&w=1600&q=80',
@@ -14,7 +14,7 @@ export class HomeComponent {
   ];
   readonly productService: ProductService = inject(ProductService);
 
-  constructor() {
-    this.productService.loadProducts();
+  async ngOnInit(): Promise<void> {
+    await this.productService.loadProducts();
   }
 }
