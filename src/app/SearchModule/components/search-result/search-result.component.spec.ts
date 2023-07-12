a6335c0afcd07302db46c5a1ac41cbdd425003ef
Daniel Morfa Vega <dmorfav@gmail.com>
2023-07-13 01:29:53 +0200

    fix testing

--- src/app/SearchModule/components/search-result/search-result.component.spec.ts
+++ src/app/SearchModule/components/search-result/search-result.component.spec.ts
@@ -1,19 +1,87 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { SearchResultComponent } from './search-result.component';
+import { ProductService } from '../../../Services/product.service';
+import { IProduct } from '../../../CoreModule/model/iproduct';
+import { WorkerCommunicationService } from '../../../Services/worker-communication.service';
+import { APP_URL } from '../../../CoreModule/helpers/constants';
 
 describe('SearchResultComponent', () => {
   let component: ComponentFixture<SearchResultComponent>;
+  let mockActivatedRoute: Partial<ActivatedRoute>;
+  let mockRouter: Partial<Router>;
+  let mockProductService: Partial<ProductService>;
+  let mockWorkerService: Partial<WorkerCommunicationService>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: []
-    });
+  const mockProducts: IProduct[] = [
+    // Mock products
+  ];
+
+  beforeEach(async () => {
+    mockActivatedRoute = {
+      queryParams: of({ title: 'mock title', category: 'mock category' })
+    };
+
+    mockRouter = {
+      navigate: jasmine.createSpy('navigate')
+    };
+
+    mockProductService = {
+      loadProductsBy: jasmine.createSpy('loadProductsBy').and.returnValue(Promise.resolve(mockProducts))
+    };
+
+    mockWorkerService = {
+      processData: jasmine.createSpy('processData').and.returnValue(Promise.resolve()),
+      categories: [] // Mock categories
+    };
+
+    await TestBed.configureTestingModule({
+      declarations: [],
+      imports: [ReactiveFormsModule, FormsModule],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: Router, useValue: mockRouter },
+        { provide: ProductService, useValue: mockProductService },
+        { provide: WorkerCommunicationService, useValue: mockWorkerService }
+      ]
+    }).compileComponents();
+
     component = TestBed.createComponent(SearchResultComponent);
     component.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load products and categories on ngOnInit', async () => {
+    await component.componentInstance.ngOnInit();
+    expect(mockProductService.loadProductsBy).toHaveBeenCalledWith({
+      title: 'mock title',
+      categoryId: 'mock category'
+    });
+    expect(mockWorkerService.processData).toHaveBeenCalledWith(mockProducts);
+    expect(component.componentInstance.categories).toEqual(mockWorkerService.categories);
+  });
+
+  it('should load products by category on category filter change', async () => {
+    const mockEvent = {
+      target: { value: 'mock category' }
+    };
+    // @ts-ignore
+    await component.componentInstance.onCategoryFilterChanged(mockEvent as Event);
+    expect(mockProductService.loadProductsBy).toHaveBeenCalledWith(component.componentInstance.searchTerms);
+  });
+
+  it('should load products on enter key press', async () => {
+    await component.componentInstance.onEnterPressed();
+    expect(mockProductService.loadProductsBy).toHaveBeenCalledWith(component.componentInstance.searchTerms);
+  });
+
+  it('should navigate to home on goBack', async () => {
+    await component.componentInstance.goBack();
+    expect(mockRouter.navigate).toHaveBeenCalledWith([APP_URL.HOME]);
   });
 });
--- src/app/SharedModule/components/product-detail/product-detail.component.spec.ts
+++ src/app/SharedModule/components/product-detail/product-detail.component.spec.ts
@@ -1,19 +1,55 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProductDetailComponent } from './product-detail.component';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { ProductService } from '../../../Services/product.service';
+import { of } from 'rxjs';
+import { IProduct } from '../../../CoreModule/model/iproduct';
+import { APP_URL } from '../../../CoreModule/helpers/constants';
+import {ICategory} from "../../../CoreModule/model/icategory";
 
 describe('ProductDetailComponent', () => {
   let component: ComponentFixture<ProductDetailComponent>;
+  let mockProductService: Partial<ProductService>;
+  let mockRouter: Partial<Router>;
+  let mockActivatedRoute: Partial<ActivatedRoute>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: []
-    });
+  const mockProduct: IProduct = {
+    category: {} as ICategory, description: "", id: 0, images: [], price: 0, title: ""
+    // Mock product data
+  };
+
+  beforeEach(async () => {
+    mockProductService = {
+      getProductById: jasmine.createSpy('getProductById').and.returnValue(Promise.resolve(mockProduct))
+    };
+
+    mockRouter = {
+      navigate: jasmine.createSpy('navigate')
+    };
+
+    mockActivatedRoute = {
+      paramMap: of({ get: (key: string) => 'mock-id' } as ParamMap)
+    };
+
+    await TestBed.configureTestingModule({
+      declarations: [],
+      providers: [
+        { provide: ProductService, useValue: mockProductService },
+        { provide: Router, useValue: mockRouter },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
+      ]
+    }).compileComponents();
+
     component = TestBed.createComponent(ProductDetailComponent);
     component.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to home on goBack', async () => {
+    await component.componentInstance.goBack();
+    expect(mockRouter.navigate).toHaveBeenCalledWith([APP_URL.HOME]);
   });
 });
