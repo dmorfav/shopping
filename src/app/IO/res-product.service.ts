8512e9264d37624264703c42f000d3588033a164
Daniel Morfa Vega <dmorfav@gmail.com>
2023-07-12 16:07:57 +0200

    feature(filter): add filters for products

--- src/app/IO/res-product.service.ts
+++ src/app/IO/res-product.service.ts
@@ -1,8 +1,9 @@
 import {inject, Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {IProduct} from "../CoreModule/model/iproduct";
 import {lastValueFrom} from "rxjs";
 import {APP_URL} from "../CoreModule/helpers/constants";
+import {IFilter} from "../CoreModule/model/ifilter";
 
 @Injectable({
   providedIn: 'root'
@@ -16,8 +17,9 @@ export class ResProductService {
   /**
    * Get all products
    */
-  getProducts(): Promise<IProduct[]> {
-    return lastValueFrom(this.http.get<IProduct[]>(`${APP_URL.API_URL}/products`));
+  getProducts(filters?: IFilter): Promise<IProduct[]> {
+    const params = this.createFilterParams(filters);
+    return lastValueFrom(this.http.get<IProduct[]>(`${APP_URL.API_URL}/products`, {params}));
   }
 
   /**
@@ -25,5 +27,20 @@ export class ResProductService {
    */
   getProductById(id: number): Promise<IProduct> {
     return lastValueFrom(this.http.get<IProduct>(`${APP_URL.API_URL}/products/${id}`));
+  }
+
+  /**
+   * Create filter parameters from the filter object
+   */
+  private createFilterParams(filters?: IFilter): HttpParams {
+    let params = new HttpParams();
+    if (filters) {
+      Object.entries(filters).forEach(([key, value]) => {
+        if (value !== undefined && value !== null) {
+          params = params.set(key, value.toString());
+        }
+      });
+    }
+    return params;
   }
 }
